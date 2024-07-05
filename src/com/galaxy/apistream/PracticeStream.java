35b83cad95b6a1db36e9f53ef788db8727d5b72b
Steven Huasupoma <steven.huasupoma@coppel.com>
2024-07-04 20:51:09 -0600

    practicando con la API Stream

---
+++ src/com/galaxy/apistream/PracticeStream.java
@@ -0,0 +1,14 @@
+package com.galaxy.apistream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PracticeStream {
+    public static void main(String[] args) {
+        List<String> nombres = Arrays.asList("Steven","Jose","Jahir");
+        nombres.stream()
+            .filter(nombre -> nombre.contains("e"))
+            .map(String::toUpperCase)
+            .forEach(System.out::println);
+    }
+}
